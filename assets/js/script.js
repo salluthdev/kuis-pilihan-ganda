cc3633a83b001bb99cff8c6a9a0599c00da694b0
Faisal L <salluthdev@gmail.com>
2022-05-15 20:15:32 +0700

    Pembuatan kerangka HTML

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// TODO
+// Membuat aplikasi kuis
+
+// 1. Membuat tampilan sederhana
+// 2. Struktur data
+// 3. Sistem pertanyaan dinamis
+// 4. Menyimpan jawaban
+// 5. Deteksi kuis selesai
+// 6. Penilaian dan perlihatkan skor
+// 7. Memperbaiki tampilan dan UX
